306400afe9ce58427a5b657cfaf220e9c3e0e927
Gustavo Andrade <gustavo_alex3000@hotmail.com>
2022-01-20 17:00:10 -0300

    feat: add submit and create task

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 import { TaskService } from '../../services/task.service';
 import { Task } from '../../Task';
 
@@ -27,5 +28,9 @@ export class TasksComponent implements OnInit {
   toggleReminder(task: Task) {
     task.reminder = !task.reminder;
     this.taskService.updateTaskReminder(task).subscribe();
+  }
+
+  addTask(task: Task) {
+    this.taskService.addTask(task).subscribe((task) => this.tasks.push(task));
   }
 }
