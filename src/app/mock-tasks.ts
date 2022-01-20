edcb0babfb0b70f9de30562b0d986221ef3a8ada
Gustavo Andrade <gustavo_alex3000@hotmail.com>
2022-01-20 13:10:33 -0300

    feat: add mock tasks and task interface

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,22 @@
+import { Task } from './Task';
+
+export const TASKS: Task[] = [
+  {
+    id: 1,
+    text: 'Doctors Appointment',
+    day: 'March 2th at 12:30pm',
+    reminder: true,
+  },
+  {
+    id: 2,
+    text: 'Meeting at School',
+    day: 'May 6th at 9:30am',
+    reminder: true,
+  },
+  {
+    id: 3,
+    text: 'Food Shopping',
+    day: 'April 9th at 6:30pm',
+    reminder: false,
+  },
+];
